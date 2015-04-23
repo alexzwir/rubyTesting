a7f238dca00e07a61ea93f7c7121d95a31b2a285
Alex Zwir <alexzwir@gmail.com>
2015-04-23 08:24:06 -0300

    Arvore genealógica

---
+++ arvoreGenealogica.rb
@@ -0,0 +1,4 @@
+puts "Familia Zwir"
+puts "Essa família é composta por muitos e muitos integrantes. Eles são:"
+puts "Digite o Nome dos seus Avós e Avôs:"
+avosEavos = gets.chomp
--- otherClass.rb
+++ otherClass.rb
@@ -1,2 +1,10 @@
 newHash = []
 puts newHash
+newHash.push "Alex"
+puts newHash
+alex = gets.chomp
+newHash.push alex
+puts newHash
+
+
+
