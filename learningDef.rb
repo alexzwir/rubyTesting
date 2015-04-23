65ab0617224e7fd530b53d99a70d83fae7a2c04d
Alex Zwir <alexzwir@gmail.com>
2015-04-23 08:08:10 -0300

    Second commit

--- learningDef.rb
+++ learningDef.rb
@@ -1,1 +1,18 @@
-learningDef.rb
+#learningDef.rb
+
+def alexIsCool
+	puts "Alex is a cool guy man, isn't he?"
+	usersAnsewr = gets.chomp
+end
+
+alexIsCool
+
+
+def vacaMujindo multiplicacaoDosMugidos
+	puts "essa vaca é mujidora "*multiplicacaoDosMugidos
+end
+
+vacaMujindo 3
+
+
+
