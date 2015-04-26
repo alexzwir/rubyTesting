e96ca35f71770139a8a081364f7c872c78aa1631
Alex Zwir <alexzwir@gmail.com>
2015-04-26 10:57:41 -0300

    rolling Dices program changes

--- rollDice.rb
+++ rollDice.rb
@@ -1,1 +1,14 @@
-Class Dice
+class Dice
+
+	def roll
+		rand(6) +1
+	end
+end
+
+#creating the dice objetcts
+dices = [Dice.new, Dice.new, Dice.new, Dice.new,Dice.new, Dice.new]
+
+
+dices.each do |diceRolling|
+	puts diceRolling.roll
+end
